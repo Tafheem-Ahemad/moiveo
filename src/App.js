32e145a2373e47a7d0ea3276b549767e76e2b3ff
Tafheem <ahemadtafheem@gmail.com>
2024-08-30 23:56:03 +0530

    creating the setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { React } from 'react';
+
+function App() {
+  return (
+    <main className='text-lg text-red-600 font-bold'>
+      React App
+    </main>
+  );
+}
+
+export default App;
